3badc72ecdf5f38738d634f9177dfbe58cb2a984
Vinnetu <chirkov.bat@gmail.com>
2018-01-29 15:22:35 +0300

    add Planet Texture

--- core/src/ru/geekbrains/stargame/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/MenuScreen.java
@@ -5,9 +5,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.engine.Base2DScreen;
 import ru.geekbrains.stargame.planet.Planet;
--- desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import ru.geekbrains.stargame.Star2DGame;
-import ru.geekbrains.stargame.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
